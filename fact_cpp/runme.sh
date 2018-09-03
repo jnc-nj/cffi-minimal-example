90ee3f04ac74b8b41ca5ea1cd02e972ccd18f407
Jack Chou <jackchou@herbmagic.cloud>
2018-09-03 17:41:19 +0800

    Bugfix.

--- fact_cpp/runme.sh
+++ fact_cpp/runme.sh
@@ -1,6 +1,6 @@
 swig -cffi -c++ -module libfact fact.i
 gcc -fpic -c fact.cpp fact_wrap.cxx
-gcc -c fact_wrap.cxx
-g++ -std=c++11 -shared fact.o fact_wrap.o -o libfact.dylib
+gcc -fpic -c fact_wrap.cxx
+g++ -std=c++11 -fpic -shared fact.o fact_wrap.o -o libfact.dylib
 cp libfact.dylib /usr/local/lib/
 ros run -l fact.lisp
